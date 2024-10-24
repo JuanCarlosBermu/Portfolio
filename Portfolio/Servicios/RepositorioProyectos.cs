1452b3534e3823fb2cdc39f46eb4758f2e41e196
JuanCarlosBermu <jcbdpad@gmail.com>
2024-10-24 13:42:37 -0500

    agregue el readme.md

---
+++ Portfolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portfolio.Models;
+
+namespace Portfolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+            new Proyecto
+            {
+                Titulo="Amazon",
+                Descripcion="Ecommerce desarrollado en ASP.NET",
+                Link="https://amazon.com",
+                ImagenURL="/Imagenes/AmazonImg.png"
+            },
+            new Proyecto
+            {
+                Titulo="Facebook",
+                Descripcion="Red Social desarrollado en PHP",
+                Link="https://facebook.com",
+                ImagenURL="/Imagenes/FacebookImg.png"
+            },
+            new Proyecto
+            {
+                Titulo="Youtube",
+                Descripcion="Plataforma digital para ver videos online",
+                Link="https://youtube.com",
+                ImagenURL="/Imagenes/YoutubeImg.png"
+            },
+            new Proyecto
+            {
+                Titulo="Steam",
+                Descripcion="Tienda en linea para comprar videojuegos",
+                Link="https://store.steampowered.com",
+                ImagenURL="/Imagenes/SteamImg.png"
+            }
+
+            };
+        }
+    }
+}
