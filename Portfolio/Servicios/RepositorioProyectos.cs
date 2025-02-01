93bf8c56404cc5c7d2f4597d95003b0b93c64a30
JuanCarlosBermu <jcbdpad@gmail.com>
2025-01-31 18:16:54 -0600

    rm del appsettingsdev

--- Portfolio/Servicios/RepositorioProyectos.cs
+++ Portfolio/Servicios/RepositorioProyectos.cs
@@ -13,31 +13,31 @@ namespace Portfolio.Servicios
             return new List<Proyecto>() {
             new Proyecto
             {
-                Titulo="Amazon",
-                Descripcion="Ecommerce desarrollado en ASP.NET",
-                Link="https://amazon.com",
-                ImagenURL="/Imagenes/AmazonImg.png"
+                Titulo="Personal Budget Manager",
+                Descripcion="Web app to track income, expenses, and optimize budget.",
+                Link="",
+                ImagenURL="/Imagenes/personal-budget-manager.png"
             },
             new Proyecto
             {
-                Titulo="Facebook",
-                Descripcion="Red Social desarrollado en PHP",
-                Link="https://facebook.com",
-                ImagenURL="/Imagenes/FacebookImg.png"
+                Titulo="Blog App",
+                Descripcion="You can create your blogs and view them all in one place.",
+                Link="",
+                ImagenURL="/Imagenes/blog-app.png"
             },
             new Proyecto
             {
-                Titulo="Youtube",
-                Descripcion="Plataforma digital para ver videos online",
-                Link="https://youtube.com",
-                ImagenURL="/Imagenes/YoutubeImg.png"
+                Titulo="Monky Brownies App",
+                Descripcion="E-commerce platform focused on selling brownies. ",
+                Link="",
+                ImagenURL="/Imagenes/monky-app.png"
             },
             new Proyecto
             {
-                Titulo="Steam",
-                Descripcion="Tienda en linea para comprar videojuegos",
-                Link="https://store.steampowered.com",
-                ImagenURL="/Imagenes/SteamImg.png"
+                Titulo="Spotilyst",
+                Descripcion="Integrated with the spotify API, gives recomendations based on your preferences",
+                Link="",
+                ImagenURL="/Imagenes/spotilyst-app.png"
             }
 
             };
